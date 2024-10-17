624277478eb79e5584b86bdc1472843a43f70336
SH327007@fi365.ort.edu.uy <SH327007@fi365.ort.edu.uy>
2024-10-17 09:36:02 -0300

    Precarga

--- LogicaNegocio/Sistema.cs
+++ LogicaNegocio/Sistema.cs
@@ -26,5 +26,32 @@
                 _clientes.Add(cliente);
             }
         }
+
+
+        private void PrecargaExcursionNacional()
+        {
+            AltaExcursionNacional("1", "Viaje corto", new DateTime(2024,06, 01), 4, 10.99, 4 , 10);
+            AltaExcursionNacional("2", "Viaje corto", new DateTime(2024, 04, 07), 7, 20.99, 7, 5);
+            AltaExcursionNacional("3", "Viaje Largo", new DateTime(2024, 06, 02), 6, 15.99, 6, 15);
+        }
+
+
+        private void PrecargaExcursionExtranjero()
+        {
+            AltaExcursionNacional("1", "Viaje corto", new DateTime(2024, 06, 01), 4, 10.99);
+            AltaExcursionNacional("2", "Viaje corto", new DateTime(2024, 04, 07), 7, 20.99);
+            AltaExcursionNacional("3", "Viaje Largo", new DateTime(2024, 06, 02), 6, 15.99);
+        }
+
+
+        private void AltaExcursionNacional(string codigo, string descripcion, DateTime fecha, int cantDias,double costoDiario,int cantMaxima, int porcentaje)
+        {
+            Nacional excursionNacional = new Nacional(codigo, descripcion, fecha, cantDias, costoDiario, cantMaxima, porcentaje);
+            if (!_excursiones.Contains(excursionNacional))
+            {
+                _excursiones.Add(excursionNacional);
+            }
+
+        }
     }
 }
