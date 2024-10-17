63ccc3fa1310d603a48f5d3e66b0182f859df390
martinleib <m@martinleib.uy>
2024-10-17 09:22:15 -0300

    Iteracion 4

---
+++ InterfazUsuario/Program.cs
@@ -0,0 +1,53 @@
+﻿using LogicaNegocio;
+using System.ComponentModel.Design;
+
+namespace InterfazUsuario
+{
+    internal class Program
+    {
+        static Sistema miSistema = new Sistema();
+
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            bool salio = false;
+            while (!salio)
+            {
+                Console.WriteLine("1- Agregar Cliente");
+                int.TryParse(Console.ReadLine(), out int ingreso);
+
+                switch (ingreso)
+                {
+                    case 1:
+                        AltaCliente();
+                        break;
+                    case 0:
+                        salio = true;
+                        break;
+                }
+            }
+        }
+
+        static void AltaCliente()
+        {
+            try { 
+                Console.WriteLine("Ingrese nombre");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese cedula");
+                string cedula = Console.ReadLine();
+                Console.WriteLine("Ingrese telefono");
+                string telefono = Console.ReadLine();
+                miSistema.AgregarCliente(cedula, nombre, telefono);
+                Console.WriteLine("Cliente creado de manera exitosa");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- LogicaNegocio/Sistema.cs
+++ LogicaNegocio/Sistema.cs
@@ -4,5 +4,27 @@
     {
         List<Cliente> _clientes = new List<Cliente>();
         List<Excursion> _excursiones = new List<Excursion>();
+
+        public void AgregarCliente(string cedula, string nombre, string tel)
+        {
+            if (string.IsNullOrEmpty(cedula))
+            {
+                throw new Exception("La cedula no es valida. Ingrese de vuelta");
+            } else if (string.IsNullOrEmpty(nombre))
+            {
+                throw new Exception("El nombre no es valido. Ingrese de vuelta");
+            } else if (string.IsNullOrEmpty(tel))
+            {
+                throw new Exception("El telefono no es valido. Ingrese de vuelta");
+            }
+
+
+            Cliente cliente = new(cedula, nombre, tel);
+
+            if (!_clientes.Contains(cliente))
+            {
+                _clientes.Add(cliente);
+            }
+        }
     }
 }
